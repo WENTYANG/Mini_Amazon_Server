70b80b185a24e7c543fb167d9ee523f1e2e73398
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-04-18 06:14:51 -0400

    finish front end

--- web_app/amazon/urls.py
+++ web_app/amazon/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('cart/', views.goto_cart, name='goto_cart'),
     path('order/', views.view_order, name='view_order'),
     path('product/<int:p_id>/', views.view_product, name='view_product'),
+    path('checkout/<int:o_id>/', views.checkout, name='checkout'),
+    path('order_detail/<int:o_id>/', views.view_order_detail, name='view_order_detail'),
+    path('change_item/<int:i_id>/', views.change_item, name='change_item'),
+    path('category/<int:c_id>/', views.view_cat, name='view_cat'),
 ]
